43cd453dcfcc0beea477b038718efad1b2174ab1
alialahmad68 <ali.alahmad.cs@gmail.com>
2023-09-18 15:10:39 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -15,4 +15,30 @@
 7-UPDATE students 
   SET Points =Points-10
   WHERE name ="Alex"
-  
+
+
+
+
+
+Part-2 Create a table  
+CREATE TABLE "graduates" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL UNIQUE,
+    "age" INTEGER,
+    "gender" TEXT,
+    "points" INTEGER,
+    "graduation" TEXT
+);
+
+ 10-INSERT INTO graduates (name, age, gender,points)
+   SELECT name, age, gender,points
+   FROM students 
+   WHERE students.Name='Layal';
+   
+ 11-UPDATE graduates 
+    SET graduation ="08/09/2018"
+    WHERE name ="Layal"
+
+ 12-DELETE FROM students WHERE students.Name="Layal"
+ 
+
