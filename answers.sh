1754aa099faf9fa4e483321e67d97832a1d956fb
alialahmad68 <ali.alahmad.cs@gmail.com>
2023-09-18 12:23:56 +0300

    Step_2 SQL Queries

---
+++ answers.sh
@@ -0,0 +1,18 @@
+1-SELECT name from students
+
+2-SELECT * FROM students WHERE Age>30
+
+3-SELECT name FROM students WHERE Gender ='F' AND Age=30
+
+4-SELECT Points FROM students WHERE name="Alex"
+
+5-INSERT INTO students(name,Age,Gender,Points) VALUES('ali',24,'M',1000)
+
+6-UPDATE students 
+  SET Points =Points+100
+  WHERE name ="Basma"
+  
+7-UPDATE students 
+  SET Points =Points-10
+  WHERE name ="Alex"
+  
